bdebe33d6d4b550c6d13db4942d8b253ae7a54f5
exitdoorcodingkinda <matthewm2608@gmail.com>
2026-04-26 20:45:31 -0500

    made some user interaction updates

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args){
-        new Pathfind();
+        int[][] visibleMap = new int[][]{
+                {0,1,0,0,0,1,0,0,0,0},
+                {0,1,0,1,0,1,0,0,0,0},
+                {0,0,0,1,0,1,0,0,0,0},
+                {0,0,0,0,0,1,0,0,0,0},
+                {1,1,0,1,0,1,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,1,1,1,0,1,0},
+                {0,0,0,0,1,0,1,0,1,0},
+                {0,0,0,0,1,0,1,0,1,0},
+                {0,0,0,0,1,0,1,0,1,0}
+        };
+        System.out.println("What method");
+        String method = new Scanner(System.in).nextLine();
+        Pathfind p = new Pathfind(visibleMap, method, 0,0,9,9);
+
+
     }
 }
--- Pathfind.java
+++ Pathfind.java
@@ -3,60 +3,49 @@ import java.util.*;
 public class Pathfind {
     private final int WIDTH = 10;
     private final int HEIGHT = 10;
-    public Pathfind(){
+    private String method;
+
+
+    public Pathfind(int[][] visibleMap, String method, int startx, int starty, int goalx, int goaly){
         Scanner sc = new Scanner(System.in);
-        int[][] visibleMap = new int[][]{
-                {0,1,0,0,0,1,0,0,0,0},
-                {0,1,0,1,0,1,0,0,0,0},
-                {0,0,0,1,0,1,0,0,0,0},
-                {0,0,0,0,0,1,0,0,0,0},
-                {1,1,0,1,0,1,0,0,0,0},
-                {0,0,0,0,0,0,0,0,0,0},
-                {0,0,0,0,1,1,1,0,1,0},
-                {0,0,0,0,1,0,1,0,1,0},
-                {0,0,0,0,1,0,1,0,1,0},
-                {0,0,0,0,1,0,1,0,1,0}
-        };
 
-            // initialize a new 2d array of nodes to have alongside our visible map, which is what displays the walls and pathing.
-        Node[][] map = new Node[HEIGHT][WIDTH];
-        for(int r = 0; r < HEIGHT; r++){
-            for(int c = 0; c < WIDTH; c++){
-                if(visibleMap[r][c] == 1){
-                    map[r][c] = new Node(null, false, r, c); // set a node to be walkable or whatever, can maybe be used for weighting in the future
+        switch(method){
+            case "astar":
+                ArrayList<Node> astarpath = findPathAStar(new int[] {startx,starty}, new int[] {goalx,goaly}, visibleMap);
+
+                for(int i = astarpath.size()-1; i >= 0; i--){
+                    System.out.println(astarpath.get(i).x + ", " + astarpath.get(i).y);
+                    visibleMap[astarpath.get(i).y][astarpath.get(i).x] = 2;
+                    printScreen(visibleMap);
+                    System.out.println("go to next iteration?");
+                    String x = sc.nextLine();
                 }
-            }
+                break;
+            case "floodfill":
+                ArrayList<Node> floodpath = findPathFloodFill(new int[] {startx,starty} ,new int[] {goalx,goaly},visibleMap);
+
+                for(int i = 0; i < floodpath.size(); i++){
+                    System.out.println(floodpath.get(i).x + ", " + floodpath.get(i).y);
+                    visibleMap[floodpath.get(i).y][floodpath.get(i).x] = 2;
+                    printScreen(visibleMap);
+                    System.out.println("go to next iteration?");
+                    String x = sc.nextLine();
+                }
+                break;
         }
-        Node goal = new Node(null, true, 9, 9);
-        goal.g = 0;
-        goal.h = 0;
-        goal.f = 0;
-        map[9][9] = goal;
-        Node start = new Node(null, true, 0, 0);
-        start.g = 0;
-        start.h = Math.abs(start.x - goal.x) + Math.abs(start.y - goal.y);
-        map[0][0] = start;
-        printScreen(visibleMap);
 
 
-        ArrayList<Node> astarpath = findPathAStar(start,goal,map);
-        ArrayList<Node> floodpath = findPathFloodFill(new int[] {0,0} ,new int[] {9,9},visibleMap);
 
-        for(int i = 0; i < floodpath.size(); i++){
-            System.out.println(floodpath.get(i).x + ", " + floodpath.get(i).y);
-            visibleMap[floodpath.get(i).y][floodpath.get(i).x] = 2;
-            printScreen(visibleMap);
-            System.out.println("go to next iteration?");
-            String x = sc.nextLine();
-        }
-        for(int i = astarpath.size()-1; i >= 0; i--){
-            System.out.println(astarpath.get(i).x + ", " + astarpath.get(i).y);
-            visibleMap[astarpath.get(i).y][astarpath.get(i).x] = 2;
-            printScreen(visibleMap);
-            System.out.println("go to next iteration?");
-            String x = sc.nextLine();
-        }
+
+
     }
+    public void printPathFloodFill(){
+
+    }
+
+
+
+
 
     public ArrayList<Node> findPathFloodFill(int[] startcoords, int[] goalcoords, int[][] map){
         Node[][] nodemap = new Node[map.length][map[0].length];
@@ -134,7 +123,23 @@ public class Pathfind {
 
     }
 
-    public ArrayList<Node> findPathAStar(Node start, Node goal, Node[][] map){
+    public ArrayList<Node> findPathAStar(int[] startcoords, int[] goalcoords, int[][] map){
+
+        Node[][] nodemap = new Node[HEIGHT][WIDTH];
+        for(int r = 0; r < HEIGHT; r++){
+            for(int c = 0; c < WIDTH; c++){
+                if(map[r][c] == 1){
+                    nodemap[r][c] = new Node(null, false, r, c); // set a node to be walkable or whatever, can maybe be used for weighting in the future
+                }
+            }
+        }
+
+        Node start = new Node(null, true, startcoords[0], startcoords[1]);
+        Node goal = new Node(null, true, goalcoords[0], goalcoords[1]);
+
+        nodemap[startcoords[1]][startcoords[0]] = start;
+        nodemap[goalcoords[1]][goalcoords[0]] = goal;
+
         PriorityQueue<Node> openList = new PriorityQueue<Node>(new Comparator<Node>() {
             @Override
             public int compare(Node o1, Node o2) {
@@ -166,11 +171,11 @@ public class Pathfind {
                 int currenty = current.y + list[1];
                 int currentx = current.x + list[0]; // set the x and y to the currents x and y + the direction
                 if(inBounds(currenty, currentx)){ // if the new coordinates are in the bounds of the visible/node map
-                    Node neighbor = map[currenty][currentx]; // get the node at neighbor in the nodemap
+                    Node neighbor = nodemap[currenty][currentx]; // get the node at neighbor in the nodemap
                     if(neighbor == null){ // if the node is not instantiated yet then instantiate
                         neighbor = new Node(null, true, currentx, currenty);
                         neighbor.g = Integer.MAX_VALUE; // so that it is default unvisited, because it hasnt been visited yet
-                        map[currenty][currentx] = neighbor; // set the location on the nodemap to the neighbors new node
+                        nodemap[currenty][currentx] = neighbor; // set the location on the nodemap to the neighbors new node
                     }
                     if(!neighbor.walkable){
                         continue; // if the node is not walkable then skip this direction
@@ -212,6 +217,10 @@ public class Pathfind {
             }
             System.out.println();
         }
+    }
+
+    public void setMethod(String method){
+        this.method = method;
     }
 
 }
